0753075f621b6303259d830fa0ad7c571a11dcba
Arjay C. Balboa <111472877+Chopyzx@users.noreply.github.com>
2022-08-18 10:25:12 +0700

    Add files via upload
    uploaded assignment 2

---
+++ assignment 2.java
@@ -0,0 +1,39 @@
+public class App {
+    public static void main(String[] args) {
+
+    
+        // VARIABLES
+        String alagako = "Choco";
+
+        // int - stores integers (whole numbers), without decimals, such as 123 or -123
+        int myAge = 18;
+
+        // float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        float myGradeInClass = 4.00f;
+
+        // char - stores single characters, such as 'a' or 'B'. Char values are
+        // surrounded by single quotes
+        char myMiddleInitial = 'C';
+
+        // boolean - stores values with two states: true or false
+        boolean iLoveVolleyball = true;
+        boolean iAmStr8 = false;
+
+        // String - stores text, such as "Hello". String values are surrounded by double
+        // quotes
+        String myFirstName = "Arjay";
+
+        // If you don't want others (or yourself) to overwrite existing values, use the
+        // final keyword (this will declare the variable as "final" or "constant", which
+        // means unchangeable and read-only)
+        final String myLastName = "Balboa";
+
+        System.out.println(myAge);
+        System.out.println(myGradeInClass);
+        System.out.println(myMiddleInitial);
+        System.out.println(iLoveVolleyball);
+        System.out.println(iAmStr8);
+        System.out.println(myFirstName);
+        System.out.println(myLastName);
+    }
+}
